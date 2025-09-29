501e53a308b28152f4619a797f223457457b2351
Fruitvliegje <n.elakel@student.han.nl>
2025-09-29 23:47:53 +0200

    Dijkstra code afgerond
    Nog zonder dir graphs een mooie toevoeging mits ik tijd heb. Commentaar toegevoegd aan code nu aan de slag met rapport opzet staat

---
+++ Rapport.md
@@ -0,0 +1,41 @@
+# Opdracht Functioneel Paradigma
+
+
+![alt text](image.png)
+
+
+"Program testing can be used to show the presence of bugs, but never to show their absence!"
+
+**— Edsger Dijkstra**
+
+
+
+
+
+
+
+
+| **Studentnaam:** | Nouri el Akel Rahhali |
+| :--- | :--- |
+| **student Nummer:** | 650002 |
+| **Course:** | APP |
+| **Datum:** | 29-09-2025 |
+| **Docent:** | Dennis Breuker |
+| **Versie:** | 1           |
+
+
+---
+
+## Inhoudsopgave
+
+1. [Inleiding](#inleiding)
+2. [Onderzoek](#onderzoek)
+3. [Challenge](#challenge)
+4. [Implementatie](#implementatie)
+5. [Reflectie](#reflectie)
+6. [Conclusie](#conclusie)
+7. [Bronvermelding](#bronvermelding)
+
+---
+
+#
---
+++ dijkstra.exs
@@ -0,0 +1,69 @@
+defmodule ShortestPath do
+  @infinity :infinity
+# Start Dijkstra
+  def shortest_path(graph, startVertex) do
+    # Initialiseer afstanden: 0 voor start vertex, :infinity voor de rest
+    distances = Map.new(Map.keys(graph), fn node ->
+      {node, (if node == startVertex, do: 0, else: @infinity)}
+    end)
+
+    queue = [{0, startVertex}]
+    dijkstra(graph, distances, queue)
+  end
+
+  # Base case: queue is leeg als pattern niet matcht? Recursieve functie
+  defp dijkstra(_graph, distances, []), do: distances
+
+  # Recursieve functie
+  defp dijkstra(graph, distances, queue) do
+
+    # check de afstand van elke node, pakt de min retourneert de gehele tuple
+    {dist, node} = Enum.min_by(queue, fn {d, _} -> d end)
+
+    # maakt een nieuwe lijst zonder de gevonden node
+    queue = List.delete(queue, {dist, node})
+
+    # Geef een lijst terug met buren van de gevonden node. Geen buren? retourneert lege lijst.
+    neighbors = Map.get(graph, node, [])
+
+
+# Loop over alle buren van de huidige node
+{distances, queue} =
+  #Reduce van lijst naar een 1 waarde (accum)
+  Enum.reduce(neighbors, {distances, queue}, fn {neighbor, cost}, {distances, queue} ->
+    # Bereken de afstand van de startnode naar deze buur via de huidige node
+    new_dist = dist + cost
+
+    # Controleer de huidige geregistreerde afstand naar deze buur
+    case distances[neighbor] do
+      # Als de buur nog niet eerder is bezocht
+      @infinity ->
+        # Update de afstand en voeg de buur toe aan de queue
+        {Map.put(distances, neighbor, new_dist), [{new_dist, neighbor} | queue]}
+
+      # Als er een korter pad hebben gevonden naar deze buur
+      current_dist when new_dist < current_dist ->
+        # Update de afstand en voeg opnieuw toe aan de queue
+        {Map.put(distances, neighbor, new_dist), [{new_dist, neighbor} | queue]}
+
+      # Het bestaande pad is al korter of gelijk, geen actie nodig
+      _ ->
+        {distances, queue}
+    end
+  end)
+
+    dijkstra(graph, distances, queue)
+  end
+end
+
+# een map met atoms (labels) als key
+graph = %{
+  a: [b: 5, c: 2],
+  b: [d: 4],
+  c: [e: 7],
+  d: [f: 1],
+  e: [f: 3],
+  f: []
+}
+
+IO.inspect ShortestPath.shortest_path(graph, :a)
